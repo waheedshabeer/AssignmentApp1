3cf7d2b12991ceb61384e27c08b126acd76b4931
David Hankins <63425832+waheedshabeer@users.noreply.github.com>
2022-06-14 03:31:36 -0400

    Update Index.js
    Added Description

--- src/navigation/Index.js
+++ src/navigation/Index.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Details from '../screens/Details';
 import Country from '../screens/Assignment3/Country';
@@ -11,17 +11,24 @@ const Stack = createNativeStackNavigator();
 
 const Index = () => {
   return (
-    <NavigationContainer  >
-    <Stack.Navigator initialRouteName='Country' screenOptions={{headerShown:false}} >
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Details" component={Details} />
-      <Stack.Screen name="Country" component={Country} />
-      <Stack.Screen name="CountryDetails" component={CountryDetails} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Country"
+        screenOptions={{headerShown: false}}>
+        {/* FOR ASSIGNMENT 3 */}
+        <Stack.Screen name="Country" component={Country} />
+        <Stack.Screen name="CountryDetails" component={CountryDetails} />
+        {/* FOR ASSIGNMENT 3 */}
 
-export default Index
+        {/* FOR ASSIGNMENT 5 */}
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+        {/* FOR ASSIGNMENT 5 */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Index;
+
+const styles = StyleSheet.create({});
